14f75394576d3c59cfb733bda4963f551d3738a3
“ijassm” <ijass.ocean@gmail.com>
2024-01-31 20:33:20 +0530

    added new file rebatch started

---
+++ intro.py
@@ -0,0 +1,34 @@
+#Basic Datatypes
+'''
+print(type("hello python"))
+print(type(12))
+print(type(12.5))
+print(type(12.5j))
+print(type(True))
+print(type(False))
+'''
+
+##Variables
+A = 20
+a = 10
+
+print(type(a))
+
+a = "hello"
+print(type(a))
+
+a = 324.5
+print(type(a))
+
+a = 534j
+print(type(a))
+
+print(a)
+print(A)
+
+name,age,height,voteEligibility = "xyz",16,5.8,False
+
+print("name: ", name)
+print("age: ", age)
+print("height: ", height)
+print("vote eligibility: ", voteEligibility)
