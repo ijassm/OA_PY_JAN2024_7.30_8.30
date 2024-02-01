537eeddb639b27dbb2f986d951030cc4b69b2da4
“ijassm” <ijass.ocean@gmail.com>
2024-02-01 20:12:22 +0530

    re-intro - 2 FEB 2024

---
+++ intro2.py
@@ -0,0 +1,20 @@
+#Basic Datatypes
+##print(type("Hello Python"))
+##print(type(123))
+##print(type(123.5))
+##print(type(123.5j))
+##print(type(True))
+##print(type(False))
+
+##Variables
+name = "xyz"
+age = 10
+height = 5.8
+voteEligiable = False
+
+print("name", name)
+print("age", age)
+print("height", height)
+print("voteEligiable", voteEligiable)
+
+
