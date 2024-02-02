2e2c8a17c4e91e23fe5c77880102e78734b66078
“ijassm” <ijass.ocean@gmail.com>
2024-02-02 20:32:43 +0530

    rebatch 3rd intro - 2 FEB 2024

---
+++ intro3.py
@@ -0,0 +1,55 @@
+##Basic Datatype
+'''
+print("sky")
+print(12)
+print(12.5)
+print(55j)
+print(True)
+print(False)
+'''
+
+##bag_name = "sky"
+##print(bag_name)
+##bag_name = "amercian"
+##bag_quantity = 3
+##bag_price = 33.5
+##bag_exists = True
+
+##print(bag_name)
+##print(bag_quantity)
+##print(bag_price)
+##print(bag_exists)
+
+##print(4 + 5)
+##print(4 - 5)
+##print(4 * 5)
+##print(5 / 2)
+##print(5 // 2)
+##print(5 % 2)
+##print(4 ** 5)
+
+#Assignment Operator
+##a = 1
+
+##a = a + 1
+##a += 1
+
+##print(a)
+
+##Comparsion operator
+
+##print(5 > 3)
+##print(5 < 3)
+##print(5 >= 3)
+##print(5 <= 3)
+##print(5 == 5)
+##print(5 != 3)
+
+##Logical operator
+
+##print(not 1 > 2 and 1 < 2)
+##print(not(1 > 2 or not 1 < 2))
+
+
+
+
