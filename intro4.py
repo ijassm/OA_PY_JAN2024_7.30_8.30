fe092db933ac8c6d3f980ae9e7e2d3755f61f4fd
“ijassm” <ijass.ocean@gmail.com>
2024-02-05 20:38:35 +0530

    rebatch intro 05 FEB 2024

---
+++ intro4.py
@@ -0,0 +1,80 @@
+##Arithmetic operator
+##print(5+234)
+##print(5-234)
+##print(55*234)
+##print(5/2)
+##print(5//2)
+##print(5%2)
+##print(5**3)
+
+##a = 4
+##a = 45.5
+##a = "hello"
+##a = True
+
+##print(a)
+
+##Assignment operator
+
+##a = 1
+
+##a = a + 1
+##a -= 1
+##print(a)
+
+##comparsion operator
+
+##print(85 > 34)
+##print(85 < 34)
+##print(85 <= 85)
+##print(85+5 >= 85)
+##print(85+5 != 90)
+##print(85+5 == 90)
+
+##Logical Operator
+
+##and or not
+
+##print(not(not(3 > 4) and 5 > 2))
+##print(3 > 4 or 5 > 2)
+##print("Hello" and "Bye")
+##print(545 or "Bye")
+
+##[]
+##()
+##{}
+##0
+##0.0
+##False
+##""
+##''
+##range(0)
+
+##identity operator
+#is is not
+
+##a = [1]
+##b = [1]
+##c = 2
+
+##print(id(a))
+##print(id(b))
+##print(id(a) == id(b))
+##print(a is b)
+##print(a == b)
+
+##Membership operator
+##in not in
+##print("34" not in "1234")
+
+print(bin(10))
+print(bin(22))
+print(10 & 22)
+print(10 | 22)
+
+print(5 >> 2)
+print(22 >> 2)
+
+
+
+
