ef6582361106c2d417e79cb8c12be39282317ebd
“ijassm” <ijass.ocean@gmail.com>
2024-02-16 20:36:33 +0530

    for loop -- 16 FEB 2024

---
+++ for-loop.py
@@ -0,0 +1,28 @@
+# range(start, stop, step)
+# range(5) start - 0, stop - 5, step - 1
+# range(1,5) start - 1, stop - 5, step - 1
+# range(0,5,2) start - 0, stop - 5, step - 2
+
+# for i in range(1, 11):
+#     print("hello", i)
+
+# even & odd
+# for i in range(1, 11):
+#     if i % 2 == 0:
+#         print("even", i)
+#     else:
+#         print("odd", i)
+
+# for i in range(1, 101):
+#     length = len(str(i))
+#     if length == 1:
+#         print(f"{i} ", end=" ")
+#     else:
+#         print(f"{i}", end=" ")
+#     if i % 10 == 0:
+#         print()
+
+# for i in range(1, 101):
+#     print("{:3d}".format(i), end=" ")
+#     if i % 10 == 0:
+#         print()
