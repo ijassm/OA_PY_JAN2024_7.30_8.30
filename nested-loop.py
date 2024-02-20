e434cf45b2b818d57fe5918aecc29a27da969078
“ijassm” <ijass.ocean@gmail.com>
2024-02-20 20:27:08 +0530

    nested loop started -- 20 FEB 2024

---
+++ nested-loop.py
@@ -0,0 +1,20 @@
+for i in range(2):
+    for j in range(2):
+        for k in range(2):
+            print(i, j, k)
+
+
+# i = 0
+###### j = 0 | j = 1
+# i = 1
+###### j = 0 | j = 1
+
+# i = 0
+# -----
+# 0 , 0
+# 0 , 1
+
+# i = 1
+# -----
+# 1 , 0
+# 1 , 1
