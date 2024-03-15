b5c2f59be699fd3b022b5e899f237306895346d4
“ijassm” <ijass.ocean@gmail.com>
2024-03-15 20:07:08 +0530

    feat : started list

---
+++ list.py
@@ -0,0 +1,51 @@
+# List
+# For storing collections of data
+
+# syntax
+# []
+
+a = [1, 2, 3, 4, 5]
+
+# index
+
+# print("positive index")
+# print(a[0], "a[0]")
+# print(a[1], "a[1]")
+# print(a[2], "a[2]")
+# print(a[3], "a[3]")
+
+# print("negative index")
+# print(a[-1], "a[-1]")
+# print(a[-2], "a[-2]")
+# print(a[-3], "a[-3]")
+# print(a[-4], "a[-4]")
+# print(a[-5], "a[-5]")
+
+# Slicing
+
+# print("Slicing")
+# print(a[1:3], "a[1:3]")
+# print(a[1:4], "a[1:4]")
+# print(a[1:5], "a[1:5]")
+# print(a[1:6], "a[1:6]")
+# print(a[::-1], "a[::-1]")
+
+# mutable
+
+# a[0] = "i"
+
+# print(a)
+
+# iterable
+
+# for i in a:
+#     print(i)
+
+# methods
+
+a.append([43, 34, 3])
+a.append(33)
+
+print(a)
+print(a[5])
+print(a[5][-1])
