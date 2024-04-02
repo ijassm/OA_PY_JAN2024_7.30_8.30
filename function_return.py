c797d9a61e6c25da8b02fb6e6f28fcc21c3b7326
“ijassm” <ijass.ocean@gmail.com>
2024-04-02 20:04:47 +0530

    Completed : function return with example

---
+++ function_return.py
@@ -0,0 +1,71 @@
+# # function return
+
+a = 6
+b = 4
+# sumOutput = 0
+# subOutput = 0
+# mulOutput = 0
+
+
+# def sum(a, b):
+#     global sumOutput
+#     sumOutput = a + b
+
+
+# def sub(a, b):
+#     global subOutput
+#     subOutput = a - b
+
+
+# def mul(a, b):
+#     global mulOutput
+#     mulOutput = a * b
+
+
+# sum(a, b)
+# sub(a, b)
+
+# mul(sumOutput, subOutput)
+
+# print(sumOutput)
+# print(subOutput)
+
+# print(mulOutput)
+
+# with return
+
+
+# def sum(a, b):
+#     return a + b
+
+
+# def sub(a, b):
+#     return a - b
+
+
+# def mul(a, b):
+#     return a * b
+
+
+# print(sum(a, b))
+# print(sub(a, b))
+
+# sumOutput = sum(a, b)
+# subOutput = sub(a, b)
+
+# print(mul(sumOutput, subOutput))
+
+l1 = [5, 6, 8, 5, 8]
+
+
+# def sum(l):
+#     output = 0
+#     for i in l:
+#         output += i
+#     return output
+
+
+# print(sum(l1))
+
+
+# print(len(l1))
